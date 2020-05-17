57eba5cddd8f531e2708b16a33b2277de92f967c
alexa0o <alexakharit@gmail.com>
2020-05-17 11:08:56 +0300

    init

---
+++ app.py
@@ -0,0 +1,16 @@
+import os
+import telebot
+from flask import Flask, requests
+
+
+TOKEN = '1245516512:AAFAPDaDe2DwPgqTZxy4eanjSpLd5VigsUg'
+bot = telebot.TeleBot(TOKEN)
+
+server = Flask(__name__)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
