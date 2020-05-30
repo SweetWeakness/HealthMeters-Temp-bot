bb5da1df4fa1eb2dc7c4fa82c3385cb6b6ce4058
alexa0o <alexakharit@gmail.com>
2020-05-30 08:56:56 +0300

    connection test2

--- api_requests.py
+++ api_requests.py
@@ -1,9 +1,10 @@
 import requests
 
 
-server_url = '151.248.122.100'
+server_url = '151.248.122.100:8000'
 
 def user_have_access(uid):
+    print(uid)
     res = requests.get(server_url + '/position', data={
         'telegram_id': uid
     })
