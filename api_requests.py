873fee492567a508ed3b3543d5d4370aa6b1c9e5
alexa0o <alexakharit@gmail.com>
2020-05-30 17:57:56 +0300

    test2

--- api_requests.py
+++ api_requests.py
@@ -5,8 +5,8 @@ server_url = 'http://151.248.122.100:8000'
 
 def user_have_access(uid):
     print(uid)
-    res = requests.post(server_url + '/position', data={
-        'telegram_id': uid
+    res = requests.post(server_url + '/position/', data={
+        'telegram_id': str(uid)
     })
     print(res.text)
     res_json = res.json()
@@ -17,20 +17,20 @@ def user_have_access(uid):
         return ''
 
 def get_manager_list(uid):
-    res = requests.get(server_url + '/list', data={
+    res = requests.get(server_url + '/list/', data={
         'telegram_id': uid
     })
     return res.json()
 
 def get_manager_stat(uid):
-    res = requests.get(server_url + '/statistic', data={
+    res = requests.get(server_url + '/statistic/', data={
         "type": "manage_statistics",
         "telegram_id": uid
     })
     return res.json()
 
 def add_worker_temp(uid, temp):
-    res = requests.post(server_url + '/user', data={
+    res = requests.post(server_url + '/user/', data={
         "telegram_id": uid,
         "temperature": str(temp)
     })
