aa1692400e7832f630912eeb032f98df6b5fdb49
alexa0o <alexakharit@gmail.com>
2020-05-30 19:00:20 +0300

    fix date

--- app.py
+++ app.py
@@ -14,9 +14,8 @@ bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
 
 def pretty_date(ugly_date):
-    date = time.strptime(ugly_date, '%Y-%m-%dT%H:%M:%SZ')
-    #return '{}.{}.{} {}:{}'.format(date.tm_mday, date.tm_mon, date.tm_year, date.tm_hour, date.tm_min)
-    return str(date)
+    date = time.strptime(ugly_date, '%Y-%m-%dT%H:%M:%S.%fZ')
+    return '{}.{}.{} {}:{}'.format(date.tm_mday, date.tm_mon, date.tm_year, date.tm_hour, date.tm_min)
 
 @bot.message_handler(commands=['start'])
 def start(message):
