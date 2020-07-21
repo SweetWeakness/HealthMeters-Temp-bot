2b5591fa4cd86bc33f5368971726f27b4bbaa1fa
SweetWeakness <shirshin.dg@phystech.edu>
2020-07-21 13:28:40 +0300

    heroku redis sucks

--- database.py
+++ database.py
@@ -3,38 +3,36 @@ import os
 
 
 class SessionsStorage:
-    uid_role: redis.Redis
-    uid_data: redis.Redis
-    uid_stage: redis.Redis
+    db: redis.Redis
 
     def __init__(self, state):
         if state == "release":
             redis_url = os.environ['REDISCLOUD_URL']
+            print(redis_url)
         else:
             redis_url = "redis://localhost:6379"
-        self.uid_role = redis.from_url(url=redis_url, db=0, decode_responses=True)
-        self.uid_stage = redis.from_url(url=redis_url, db=1, decode_responses=True)
+        self.db = redis.from_url(url=redis_url, db=0, decode_responses=True)
 
     def get_role(self, uid: int) -> str:
-        return self.uid_role.get(str(uid))
+        return self.db.get(str(uid))
 
     def set_role(self, uid: int, role: str) -> None:
-        self.uid_role.set(str(uid), str(role))
+        self.db.set(str(uid), str(role))
 
     def set_stage(self, uid: int, stage: str) -> None:
-        self.uid_stage.set(str(uid), str(stage))
+        self.db.set(str(uid) + " stage", str(stage))
 
     def get_stage(self, uid: int) -> str:
-        return self.uid_stage.get(str(uid))
+        return self.db.get(str(uid) + " stage")
 
-    def get_role_data(self, uid: int, role: str) -> str:
-        return self.uid_stage.get(str(uid) + str(role))
+    def get_data(self, uid: int) -> str:
+        return self.db.get(str(uid) + " data")
 
-    def set_role_data(self, uid: int, role: str, data: str) -> None:
-        self.uid_stage.set(str(uid) + str(role), str(data))
+    def set_data(self, uid: int, data: str) -> None:
+        self.db.set(str(uid) + " data", str(data))
 
     def role_exist(self, uid: int) -> bool:
-        return self.uid_role.exists(uid)
+        return self.db.exists(str(uid))
 
     def stage_exist(self, uid: int) -> bool:
-        return self.uid_stage.exists(uid)
+        return self.db.exists(str(uid) + " stage")
