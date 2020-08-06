b2ed7d672c474a2123b119a4b2d0b8615e20fb51
SweetWeakness <shirshin.dg@phystech.edu>
2020-08-06 20:53:49 +0300

    heroku reset up

--- app/databases/models.py
+++
@@ -1,6 +0,0 @@
-from app import db
-
-
-class User(db.Model):
-    guid = db.Column(db.String(36), primary_key=True)
-    username = db.Column(db.String(32), index=True)
--- run.py
+++
@@ -1,9 +0,0 @@
-from app import server
-from app.config import config_manager as cfg
-
-
-server_host = cfg.get_server_host()
-server_port = cfg.get_server_port()
-
-
-server.run(threaded=True, host=server_host, port=server_port)
